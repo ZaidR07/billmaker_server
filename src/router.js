2e57ff18716d1bc600000cb0125533b51ca89cc1
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-01-30 20:08:20 +0530

    pre-production

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createpdf } from "./controllers/pdf.js";
+import { createpdf , downloadPDF } from "./controllers/pdf.js";
 
 const router = Router();
 
 router.post("/api/createpdf", createpdf);
+router.post("/api/downloadpdf",downloadPDF);
+
 
 export default router
