dd22eee2e73c6de552003df3be4e719db4388ee4
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2025-02-10 20:42:05 +0530

    changed the pdf table format8

--- src/controllers/pdf.js
+++ src/controllers/pdf.js
@@ -81,7 +81,7 @@ export const createpdf = async (req, res) => {
       head: [datecolumns], // Table headers
       body: daterows, // Table data
       startY: yPosition,
-      margin: { left: pageWidth - 80 , right : 20 }, // Position of the table
+      margin: { left: pageWidth - 80 , right : 18 }, // Position of the table
       theme: "grid",
 
       columnStyles: {
